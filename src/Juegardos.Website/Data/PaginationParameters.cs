e8e6c1280c9a311441e37b84dbed81c466acb4b8
German De Francesco <germandefrancesco@hotmail.com>
2022-05-26 19:30:53 -0300

    implemented pagination component, fixed background and navbar colors

---
+++ src/Juegardos.Website/Data/PaginationParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace Juegardos.Website.Data
+{
+    public class PaginationParameters
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+        public int TotalCount { get; set; } = -1;
+        public int Offset => (PageNumber - 1) * PageSize;
+        public int TotalPages => TotalCount / PageSize;
+        public bool RemainingItems => TotalCount < 0 || (PageNumber - 1) * PageSize < TotalCount;
+    }
+}
