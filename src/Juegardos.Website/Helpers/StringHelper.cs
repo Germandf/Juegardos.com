e0762485da8e51aa236619fd58bb6d1abc2e8077
German De Francesco <germandefrancesco@hotmail.com>
2022-05-26 21:16:44 -0300

    fix carouselmultiple behaviour when there are many carousels in one page

---
+++ src/Juegardos.Website/Helpers/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Juegardos.Website.Helpers;
+
+public static class StringHelper
+{
+    private static readonly Random _random = new();
+    public static string RandomString(int length = 30)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[_random.Next(s.Length)]).ToArray());
+    }
+}
--- src/Juegardos.Website/wwwroot/js/carousel.js
+++ src/Juegardos.Website/wwwroot/js/carousel.js
@@ -1,25 +1,25 @@
-﻿window.scrollL = () => {
-    let x = document.getElementsByClassName("carousel-posters")[0];
+﻿window.scrollL = (carouselId) => {
+    let x = document.getElementById(carouselId).getElementsByClassName("carousel-posters")[0];
     let step = window.outerWidth / 2;
     x.scrollLeft -= step;
 }
 
-window.scrollR = () => {
-    let x = document.getElementsByClassName("carousel-posters")[0];
+window.scrollR = (carouselId) => {
+    let x = document.getElementById(carouselId).getElementsByClassName("carousel-posters")[0];
     let step = window.outerWidth / 2;
     x.scrollLeft += step;
 }
 
-window.getScrollVal = () => {
+window.getScrollVal = (carouselId) => {
     setTimeout(() => {
-        let x = document.getElementsByClassName("carousel-posters")[0];
-        let el = document.getElementsByClassName("left-arrow")[0];
+        let x = document.getElementById(carouselId).getElementsByClassName("carousel-posters")[0];
+        let el = document.getElementById(carouselId).getElementsByClassName("left-arrow")[0];
         if (x.scrollLeft == 0) {
             el.style.display = "none";
         } else {
             el.style.display = "flex";
         }
-        let el2 = document.getElementsByClassName("right-arrow")[0];
+        let el2 = document.getElementById(carouselId).getElementsByClassName("right-arrow")[0];
         let right = x.scrollWidth - (x.scrollLeft + x.clientWidth) + 1;
         if (right <= 2) {
             el2.style.display = "none";
