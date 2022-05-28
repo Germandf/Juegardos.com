4b91b190d5c5636b66c37260c9f15f77e4a609a3
German De Francesco <germandefrancesco@hotmail.com>
2022-05-28 15:50:02 -0300

    implemented ads

---
+++ src/Juegardos.Website/Data/AdType.cs
@@ -0,0 +1,10 @@
+﻿namespace Juegardos.Website.Data;
+
+public enum AdType
+{
+    Ad160x600,
+    Ad300x250,
+    Ad300x600,
+    Ad728x90,
+    Ad970x90
+}
--- src/Juegardos.Website/Data/Game.cs
+++ src/Juegardos.Website/Data/Game.cs
@@ -1,10 +1,9 @@
-﻿namespace Juegardos.Website.Data
+﻿namespace Juegardos.Website.Data;
+
+public class Game
 {
-    public class Game
-    {
-        public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
-        public bool IsFavourite { get; set; }
-        public string Image { get; set; } = "https://image.tmdb.org/t/p/original///56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
-    }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public bool IsFavourite { get; set; }
+    public string Image { get; set; } = "https://image.tmdb.org/t/p/original///56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
 }
