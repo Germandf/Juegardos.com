d238751c46bb7039226c4b4ad35419092f54760d
German De Francesco <germandefrancesco@hotmail.com>
2022-05-29 20:49:58 -0300

    added url route parameter in Gameplay page

--- src/Juegardos.Website/Data/Game.cs
+++ src/Juegardos.Website/Data/Game.cs
@@ -2,7 +2,7 @@
 
 public class Game
 {
-    public string Title { get; set; } = "";
+    public string Name { get; set; } = "";
     public string Description { get; set; } = "";
     public bool IsFavourite { get; set; }
     public string Image { get; set; } = "https://image.tmdb.org/t/p/original///56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
