fd0934c0a047921a14fa009975876eb5ecab6609
Gabriel <gaballone@hotmail.com>
2022-05-31 00:09:21 -0300

    Added User class and User services. User page no loading data

---
+++ src/Juegardos.Website/Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Juegardos.Website.Data
+{
+    public class User
+    {
+        public string Name { get; set; } = "";
+        public string Email{ get; set; } = "";
+        public string TimePlayed { get; set; } = "";
+        public string Image { get; set; } = "https://image.tmdb.org/t/p/original///56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
+        public string FavGames { get; set; } = "";
+        public string MemberSince { get; set; } = "";
+    }
+}
---
+++ src/Juegardos.Website/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Juegardos.Website.Data;
+
+namespace Juegardos.Website.Services;
+
+
+public class UserService : IUserService
+{
+    private readonly User _user = new()
+    {
+        Name = "Juan Gonzales",
+        Email = "juna_gonzales@mail.com",
+        FavGames = "",
+        TimePlayed = "32,4 horas",
+        MemberSince = "14/02/2021"
+    };
+
+    public async Task<User?> GetUser()
+    {
+        await Task.Delay(1000);
+        return _user;
+    }
+}
+
+
+
