1060983c7180d4bc369a9027b803f1c3db965a82
German De Francesco <germandefrancesco@hotmail.com>
2022-06-06 18:56:37 -0300

    fix navbar search urls not working

--- src/Juegardos.Website/Services/IGameService.cs
+++ src/Juegardos.Website/Services/IGameService.cs
@@ -5,7 +5,7 @@ namespace Juegardos.Website.Services;
 public interface IGameService
 {
     Task<List<GameCategory>> GetCategoriesWithGames();
-    Task<List<Game>> SearchGames(string nameOrCategory, GameFilters? filters = null);
+    Task<List<Game>> SearchGames(string name, GameFilters? filters = null);
     Task<Game?> GetGame(string name);
     Task<Game?> GetOutstandingGame();
     Task<List<Game>> GetFavouriteGamesFromUser();
