e5fd4fff980a69539e5938e039e34b0274e0a564
German De Francesco <germandefrancesco@hotmail.com>
2022-06-15 19:18:40 -0300

    board class refactoring

--- src/GermanTest2/board.js
+++ src/GermanTest2/board.js
@@ -1,24 +1,27 @@
+const chipsContainerWidth = 300;
+
 class Board {
-    constructor(boardCanvas, boardCtx) {
+    constructor(boardCanvas, boardCtx, horizontalChips, verticalChips) {
         this.boardCanvas = boardCanvas;
         /** @type {CanvasRenderingContext2D} */
         this.boardCtx = boardCtx;
-        this.cantCeldasHorizontal = 7;
-        this.cantCeldasVertical = 6;
-        this.celda = boardCanvas.width / ((this.cantCeldasHorizontal + this.cantCeldasVertical) / 2);
-        this.mitadCelda = this.celda / 2;
+        this.horizontalChips = horizontalChips;
+        this.verticalChips = verticalChips;
+        this.celda = boardCanvas.width / ((this.horizontalChips + this.verticalChips) / 2);
     }
 
     drawBoard() {
-        this.boardCanvas.height = this.celda * this.cantCeldasVertical;
-        this.boardCanvas.width = this.celda * this.cantCeldasHorizontal;
+        // draw board background
+        this.boardCanvas.height = this.celda * this.verticalChips;
+        this.boardCanvas.width = this.celda * this.horizontalChips + chipsContainerWidth * 2;
         this.boardCtx.fillStyle = "#3867d6";
         this.boardCtx.fillRect(0, 0, boardCanvas.width, boardCanvas.height);
         this.boardCtx.stroke();
+        // draw board holes
         this.boardCtx.fillStyle = "white";
-        for (var y = 0; y < this.cantCeldasVertical * this.celda; y += this.celda) {
-          for (var x = 0; x < this.cantCeldasHorizontal * this.celda; x += this.celda) {
-            this.boardCtx.arc(x + this.mitadCelda, y + this.mitadCelda, this.celda * 0.35, 0, Math.PI * 2, true);
+        for (var y = 0; y < this.verticalChips * this.celda; y += this.celda) {
+          for (var x = chipsContainerWidth; x < this.horizontalChips * this.celda + chipsContainerWidth; x += this.celda) {
+            this.boardCtx.arc(x + this.celda / 2, y + this.celda / 2, this.celda * 0.35, 0, Math.PI * 2, true);
             this.boardCtx.fill();
             this.boardCtx.closePath();
           }
--- src/GermanTest2/main.js
+++ src/GermanTest2/main.js
@@ -2,7 +2,7 @@ var boardCanvas = document.getElementById("board");
 /** @type {boardCanvasRenderingContext2D} */
 var boardCtx = boardCanvas.getContext("2d");
 
-let board = new Board(boardCanvas, boardCtx);
+let board = new Board(boardCanvas, boardCtx, 7, 6);
 board.drawBoard();
 
 let lastClickedChip = null;
