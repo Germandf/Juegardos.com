04503e7a5d8bed45e147a5cd2baca7e55e886ad9
Germandf <69018178+Germandf@users.noreply.github.com>
2022-06-21 20:07:47 +0000

    Deploying to gh-pages from  @ 1abc4a050681343975fd15265ea98faf2e87b8fc 🚀

--- js/connect4/main.js
+++ js/connect4/main.js
@@ -1,28 +1,32 @@
-const boardCanvas = document.getElementById("board");
-/** @type {boardCanvasRenderingContext2D} */
-const boardCtx = boardCanvas.getContext("2d");
-const mode = document.getElementById("mode");
-const playerTurnElement = document.getElementById("playerTurn");
-const timerElement  = document.getElementById("timer");
-const resultElement  = document.getElementById("result");
-const board = new Board(boardCanvas, boardCtx, playerTurnElement, timerElement, resultElement);
-const resetBtn = document.getElementById("reset");
+window.startConnect4 = () => {
+    const boardCanvas = document.getElementById("board");
+    /** @type {boardCanvasRenderingContext2D} */
+    const boardCtx = boardCanvas.getContext("2d");
+    const mode = document.getElementById("mode");
+    const playerTurnElement = document.getElementById("playerTurn");
+    const timerElement = document.getElementById("timer");
+    const resultElement = document.getElementById("result");
+    const board = new Board(boardCanvas, boardCtx, playerTurnElement, timerElement, resultElement);
+    const resetBtn = document.getElementById("reset");
 
-board.setUpBoard(7, 6, 4, 5 * 60);
-resetBtn.addEventListener('click', function () {
-    let modeSelected = mode.value;
-    switch (modeSelected) {
-        case "3":
-            board.setUpBoard(6, 5, 3, 5 * 60);
-            break;
-        case "5":
-            board.setUpBoard(8, 7, 5, 5 * 60);
-            break;
-        case "6":
-            board.setUpBoard(9, 8, 6, 5 * 60);
-            break;
-        default:
-            board.setUpBoard(7, 6, 4, 5 * 60);
-            break;
-    }
-});
+    board.setUpBoard(7, 6, 4, 5 * 60);
+    resetBtn.addEventListener('click', function () {
+        let modeSelected = mode.value;
+        switch (modeSelected) {
+            case "3":
+                board.setUpBoard(6, 5, 3, 5 * 60);
+                break;
+            case "5":
+                board.setUpBoard(8, 7, 5, 5 * 60);
+                break;
+            case "6":
+                board.setUpBoard(9, 8, 6, 5 * 60);
+                break;
+            default:
+                board.setUpBoard(7, 6, 4, 5 * 60);
+                break;
+        }
+    });
+
+}
+
