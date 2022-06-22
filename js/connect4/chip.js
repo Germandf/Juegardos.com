d68cdd3a778e01618a83d3012a2f657b3e700b55
Germandf <69018178+Germandf@users.noreply.github.com>
2022-06-22 22:29:49 +0000

    Deploying to gh-pages from  @ e8c3ea35a9a1f28e9ee65ad31158bccd657d256e 🚀

--- js/connect4/chip.js
+++ js/connect4/chip.js
@@ -5,8 +5,6 @@ class Chip {
         this.radius = radius;
         /** @type {CanvasRenderingContext2D} */
         this.ctx = ctx;
-        this.highlighted = false;
-        this.highlightedStyle = "black";
         const img = new Image();
         this.loadedImg = false;
         this.player = player;
@@ -21,19 +19,23 @@ class Chip {
         }
     }
 
+    // sets filling color
     setFill(fill) {
         this.fill = fill;
     }
 
+    // gets filling color
     getFill() {
         return this.fill;
     }
 
+    // sets chip's position
     setPosition(x, y) {
         this.x = x;
         this.y = y;
     }
 
+    // gets chip's position
     getPosition() {
         return {
             x: this.x,
@@ -41,16 +43,12 @@ class Chip {
         }
     }
 
+    // redraw chip on canvas' context
     draw() {
         this.ctx.beginPath();
         this.ctx.fillStyle = this.fill;
         this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         this.ctx.fill();
-        if (this.highlighted === true) {
-            this.ctx.strokeStyle = this.highlightedStyle;
-            this.ctx.lineWidth = 2;
-            this.ctx.stroke();
-        }
         if (this.loadedImg) {
             this.ctx.drawImage(this.img, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
         }
@@ -63,14 +61,12 @@ class Chip {
         this.ctx.closePath();
     }
 
+    // gets chip's radius
     getRadius() {
         return this.radius;
     }
 
-    setHighlighted(highlighted) {
-        this.highlighted = highlighted;
-    }
-
+    // gets if mouse is on chip's position
     isMouseOn(x, y) {
         let _x = this.x - x;
         let _y = this.y - y;
@@ -78,6 +74,7 @@ class Chip {
         return sqrt < this.radius;
     }
 
+    // gets chip's owner player
     getPlayer(){
         return this.player;
     }
