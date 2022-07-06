a12a56dfedaff846b9bd8f7cbb75b3e6f3d2b4ac
German De Francesco <germandefrancesco@hotmail.com>
2022-07-06 18:40:25 -0300

    player jumping animation now updates position

--- src/RunnerVanilla/main.js
+++ src/RunnerVanilla/main.js
@@ -1,18 +1,39 @@
 let character = document.getElementById("character");
+let skull = document.getElementById("skull");
+let horse = document.getElementById("horse");
+let claw = document.getElementById("claw");
+let jumping = false;
+
+character.style.bottom = "0px";
+skull.style.right = "96px";
+horse.style.right = "96px";
 
 window.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowUp") {
+    if (event.key === "ArrowUp" && !jumping) {
         character.className = "jumping";
+        jumping = true;
+        setTimeout(() => {
+            character.className = "running";
+            jumping = false;
+        }, 1000)
     } else if (event.key === "ArrowLeft") {
         character.className = "taking-damage";
     } else if (event.key === "ArrowRight") {
         character.className = "dying";
-    } else if (event.key === "ArrowDown") {
-        character.className = "running";
     }
-}); 
+});
 
 let intervalId = setInterval(function() 
 {
-    console.log("gameloop");
+    var right = parseInt(skull.style.right);
+    skull.style.right = (right + 10) + "px";
+    horse.style.right = (right + 10) + "px";
+    /*
+    if (character.offsetLeft == skull.offsetLeft - character.clientWidth + skull.clientWidth){
+        clearInterval(intervalId);
+    }
+    */
+    if (character.offsetLeft == horse.offsetLeft - character.clientWidth + horse.clientWidth){
+        clearInterval(intervalId);
+    }
 }, 50);
