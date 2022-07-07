2a799365bf0486f6e2b7eed29051307ab552cf22
German De Francesco <germandefrancesco@hotmail.com>
2022-07-06 23:47:40 -0300

    enemy spawning working!

--- src/RunnerVanilla/main.js
+++ src/RunnerVanilla/main.js
@@ -1,12 +1,12 @@
 let character = document.getElementById("character");
-let skull = document.getElementById("skull");
-let horse = document.getElementById("horse");
-let claw = document.getElementById("claw");
 let jumping = false;
 let dead = false;
 let lives = 3;
 let takingDamage = false;
 let canJump = true;
+let enemies = [];
+
+createEnemy();
 
 window.addEventListener("keydown", (event) => {
     if (dead) return;
@@ -19,42 +19,62 @@ window.addEventListener("keydown", (event) => {
                 jumping = false;
             }
         }, 1125)
-    } else if (event.key === "ArrowLeft") {
-        character.className = "taking-damage";
-    } else if (event.key === "ArrowRight") {
-        character.className = "dying";
     }
 });
 
 let intervalId = setInterval(function() 
 {
-    var disBtwCharAndSkull = character.offsetLeft - skull.offsetLeft;
-    var disBtwCharAndHorse = character.offsetLeft - horse.offsetLeft;
-    if (((disBtwCharAndSkull > -150 && disBtwCharAndSkull < 20) && jumping) ||
-        ((disBtwCharAndHorse > -180 && disBtwCharAndHorse < 70) && !jumping)){
-        if (!takingDamage){
-            lives--;
-            if (lives <= 0){
-                clearInterval(intervalId);
-                const animations = document.querySelectorAll('[data-pauseable');
-                animations.forEach(animation => {
-                    animation.style.animationPlayState = 'paused';
-                });
-                dead = true;
-                character.className = "dying";
-                character.style.animationPlayState = 'running';
-            } else {
-                takingDamage = true;
-                canJump = false;
-                character.className = "taking-damage";
-                setTimeout(() => {
-                    character.className = "running";
-                    canJump = true;
-                }, 250);
-                setTimeout(() => {
-                    takingDamage = false;
-                }, 1000);
-            }
+    enemies.forEach(enemy => {
+        var distance = character.offsetLeft - enemy.offsetLeft;
+        if ((enemy.classList.contains("skull") && distance > -150 && distance < 20 && jumping) ||
+            (enemy.classList.contains("horse") && distance > -180 && distance < 70 && !jumping)){
+                enemyTouchingPlayer();
+        }
+        if (enemy.offsetLeft < -200){
+            enemies.pop(enemy);
+        }
+    });
+}, 50);
+
+function createEnemy() {
+    if (!dead) {
+        var randomTime = Math.floor(Math.random() * 3000) + 1000;
+        var randomEnemy = Math.floor(Math.random() * 2);
+        const newDiv = document.createElement("div");
+        if (randomEnemy == 1)
+            newDiv.classList.add('skull');
+        else
+            newDiv.classList.add('horse');
+        newDiv.dataset.pauseable = "";
+        document.querySelector(".runner-container").appendChild(newDiv);
+        enemies.push(newDiv);
+        setTimeout(() => { createEnemy() }, randomTime);
+    }
+}
+
+function enemyTouchingPlayer() {
+    if (!takingDamage){
+        lives--;
+        if (lives <= 0){
+            clearInterval(intervalId);
+            const animations = document.querySelectorAll('[data-pauseable');
+            animations.forEach(animation => {
+                animation.style.animationPlayState = 'paused';
+            });
+            dead = true;
+            character.className = "dying";
+            character.style.animationPlayState = 'running';
+        } else {
+            takingDamage = true;
+            canJump = false;
+            character.className = "taking-damage";
+            setTimeout(() => {
+                character.className = "running";
+                canJump = true;
+            }, 250);
+            setTimeout(() => {
+                takingDamage = false;
+            }, 800);
         }
     }
-}, 50);
+}
