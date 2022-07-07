7e6eeb3baf65c9808bfa65a8044654cb474583bd
German De Francesco <germandefrancesco@hotmail.com>
2022-07-06 23:16:23 -0300

    implemented taking damage

--- src/RunnerVanilla/main.js
+++ src/RunnerVanilla/main.js
@@ -4,10 +4,13 @@ let horse = document.getElementById("horse");
 let claw = document.getElementById("claw");
 let jumping = false;
 let dead = false;
+let lives = 3;
+let takingDamage = false;
+let canJump = true;
 
 window.addEventListener("keydown", (event) => {
     if (dead) return;
-    if (event.key === "ArrowUp" && !jumping) {
+    if (event.key === "ArrowUp" && !jumping && canJump) {
         character.className = "jumping";
         jumping = true;
         setTimeout(() => {
@@ -29,13 +32,29 @@ let intervalId = setInterval(function()
     var disBtwCharAndHorse = character.offsetLeft - horse.offsetLeft;
     if (((disBtwCharAndSkull > -150 && disBtwCharAndSkull < 20) && jumping) ||
         ((disBtwCharAndHorse > -180 && disBtwCharAndHorse < 70) && !jumping)){
-        clearInterval(intervalId);
-        const animations = document.querySelectorAll('[data-pauseable');
-        animations.forEach(animation => {
-            animation.style.animationPlayState = 'paused';
-        });
-        dead = true;
-        character.className = "dying";
-        character.style.animationPlayState = 'running';
+        if (!takingDamage){
+            lives--;
+            if (lives <= 0){
+                clearInterval(intervalId);
+                const animations = document.querySelectorAll('[data-pauseable');
+                animations.forEach(animation => {
+                    animation.style.animationPlayState = 'paused';
+                });
+                dead = true;
+                character.className = "dying";
+                character.style.animationPlayState = 'running';
+            } else {
+                takingDamage = true;
+                canJump = false;
+                character.className = "taking-damage";
+                setTimeout(() => {
+                    character.className = "running";
+                    canJump = true;
+                }, 250);
+                setTimeout(() => {
+                    takingDamage = false;
+                }, 1000);
+            }
+        }
     }
 }, 50);
