aa658bf5378a250e70af817499f92b76d949bdd1
German De Francesco <germandefrancesco@hotmail.com>
2022-07-10 19:21:31 -0300

    fix connect4 not working

--- src/Juegardos.Website/Data/Game.cs
+++ src/Juegardos.Website/Data/Game.cs
@@ -8,12 +8,13 @@ public class Game
     public string Image { get; set; } = "https://image.tmdb.org/t/p/original///56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
     public bool IsOutstanding { get; set; }
     public string Category { get; set; } = "";
-    public int Likes { get; set; }
-    public int Dislikes { get; set; }
+    public int Likes { get; set; } = new Random().Next(50, 5000);
+    public int Dislikes { get; set; } = new Random().Next(8, 500);
     public LikeDecision UserLikeDecision { get; set; } = LikeDecision.None;
     public string HowToPlay { get; set; } = "Tu misión es infiltrarte en las tropas enemigas y eliminar tantos soldados como sea posible. Contás con munición limitada, pero podés conseguir más comprando en la tienda cada vez que el juego así lo permita.";
     public List<string> Controls { get; set; } = new() { "WASD - Moverse", "Click izquierdo - Disparar", "Click derecho - Apuntar", "R - Recargar", "Espacio - Saltar", "Q - Cambio de arma" };
     public string Video { get; set; } = "deadcells-trailer.mp4";
+    public HardcodedGame HardcodedGame { get; set; } = HardcodedGame.None;
 }
 
 public enum LikeDecision
@@ -21,4 +22,11 @@ public enum LikeDecision
     None,
     Like,
     Dislike
+}
+
+public enum HardcodedGame
+{
+    None,
+    Connect4,
+    SamuraiApocalypse
 }
