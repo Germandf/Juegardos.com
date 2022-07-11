39e33047ff5585dfac2de83be680a7493ed3d1a5
German De Francesco <germandefrancesco@hotmail.com>
2022-07-11 19:32:46 -0300

    implemented fullscreen button

---
+++ src/Juegardos.Website/wwwroot/js/utilities.js
@@ -0,0 +1,13 @@
+﻿window.toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+window.copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+}
